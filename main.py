4f1130c94d47c44c68c4083ceb67fd295a2943c5
geoscotton <geo.scotton@gmail.com>
2025-05-16 16:32:37 -0300

    saving

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ from pyspark.pandas import DataFrame as ps
 from pyspark.sql import Window
 from pathlib import Path
 import logging
-
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    f1_score,
+    accuracy_score,
+    roc_auc_score,
+)
+import pickle
 from src.processors import OffersProcessor, ProfilesProcessor, TransactionsProcessor
 
 logging.basicConfig(
@@ -76,7 +89,6 @@ def merge_data():
 
 
 def build_dataset():
-    logger.info("Carregando dados processados")
     transactions_full = spark.read.json(
         (PROCESSED_DATA_PATH / "transactions_full").as_posix()
     )
@@ -315,7 +327,153 @@ def build_dataset():
     return None
 
 
+def train_model():
+    logger.info("Carregando dados de modelagem")
+    df = spark.read.json(
+        (PROCESSED_DATA_PATH / "modelling_dataset").as_posix()
+    ).toPandas()
+
+    logger.info("Separando dataset em treino e teste")
+    # pegando ultimas offers de cada
+    last_offers_idx = (
+        df.sort_values(["account_id", "time_since_test_start"])
+        .groupby("account_id")
+        .tail(1)
+        .index
+    )
+
+    # criando datasets
+    df_test = df.loc[last_offers_idx].copy()
+    df_train = df.drop(last_offers_idx).copy()
+
+    X_train = df_train.drop(columns=["target"])
+    y_train = df_train["target"]
+    X_test = df_test.drop(columns=["target"])
+    y_test = df_test["target"]
+
+    # Criando transformers que serão usadas no modelo
+    simplescaler_features = [
+        "discount_value",
+        "duration",
+        "email",
+        "min_value",
+        "mobile",
+        "qtd_canais",
+        "registered_on_cos",
+        "registered_on_seno",
+        "social",
+        "web",
+    ]
+    simplescaler_transformer = Pipeline(steps=[("scaler", StandardScaler())])
+
+    numeric_median_features = ["age", "credit_card_limit"]
+    numeric_median_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="median")),
+            ("scaler", StandardScaler()),
+        ]
+    )
+
+    numeric_0_features = [
+        "num_past_viewed",
+        "past_offer_conversion",
+        "total_past_amount",
+        "total_past_reward",
+    ]
+    numeric_0_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="constant", fill_value=0)),
+            ("scaler", StandardScaler()),
+        ]
+    )
+
+    numeric_1_features = ["time_since_last_offer"]
+    numeric_1_transformer = Pipeline(
+        steps=[
+            ("imputer", SimpleImputer(strategy="constant", fill_value=-1)),
+            ("scaler", StandardScaler()),
+        ]
+    )
+
+    categorical_features = [
+        "gender",
+        "offer_type",
+        "year_registered",
+        "month_registered",
+    ]
+    categorical_transformer = Pipeline(
+        steps=[
+            (
+                "encoder",
+                OneHotEncoder(
+                    handle_unknown="infrequent_if_exist", sparse_output=False
+                ),
+            ),
+        ]
+    )
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("num_median", numeric_median_transformer, numeric_median_features),
+            ("num_0", numeric_0_transformer, numeric_0_features),
+            ("num_1", numeric_1_transformer, numeric_1_features),
+            ("cat", categorical_transformer, categorical_features),
+            ("simplescaler", simplescaler_transformer, simplescaler_features),
+        ],
+        remainder="drop",
+        verbose_feature_names_out=False,
+    )
+    logger.info("Treinando modelo HistGradientBoostingClassifier com os hiperparametros já otimizados a seguir:")
+    params = {'class_weight': 'balanced', 'l2_regularization': 0.003439284699685042, 'max_depth': 9, 'max_leaf_nodes': 129}
+    logger.info(params)
+    model = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("classifier", HistGradientBoostingClassifier(**params)),
+             ])
+    
+    model.fit(X_train, y_train)
+    logger.info("Modelo treinado com sucesso")
+    
+    y_train_pred = model.predict(X_train)
+    y_test_pred = model.predict(X_test)
+
+    y_train_proba = model.predict_proba(X_train)[:,1]
+    y_test_proba = model.predict_proba(X_test)[:,1]
+
+    train_precision = precision_score(y_train, y_train_pred)
+    train_recall = recall_score(y_train, y_train_pred)
+    train_f1 = f1_score(y_train, y_train_pred)
+    train_accuracy = accuracy_score(y_train, y_train_pred)
+    train_roc_auc = roc_auc_score(y_train, y_train_proba)
+
+    test_precision = precision_score(y_test, y_test_pred)
+    test_recall = recall_score(y_test, y_test_pred)
+    test_f1 = f1_score(y_test, y_test_pred)
+    test_accuracy = accuracy_score(y_test, y_test_pred)
+    test_roc_auc = roc_auc_score(y_test, y_test_proba)
+
+    metrics_df = pd.DataFrame({
+        'Train': [train_precision, train_recall, train_f1, train_accuracy, train_roc_auc],
+        'Test': [test_precision, test_recall, test_f1, test_accuracy, test_roc_auc]
+    }, index=['Precision', 'Recall', 'F1', 'Accuracy', 'ROC AUC'])
+
+    (REPO_PATH / 'models_artefact').mkdir(parents=True, exist_ok=True)
+    
+    logger.info("\nModel Performance Metrics:")
+    logger.info(f"\n{metrics_df.round(3)}")
+    metrics_df.to_csv((REPO_PATH / 'models_artefact' / 'metrics.csv').as_posix(), index=True)
+    
+    logger.info("Salvando modelo")
+    
+
+    with open((REPO_PATH / "models_artefact" / "model.pkl").as_posix(), "wb") as f:
+        pickle.dump(model, f)
+    logger.info(f"Modelo salvo com sucesso em {(REPO_PATH / "models_artefact" / "model.pkl").as_posix()}")
+
+
 if __name__ == "__main__":
     preprocessa_input_data()
     merge_data()
     build_dataset()
+    train_model()
